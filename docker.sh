0de6835dfbfa1f908b34420e6518f00464adacec
maewnarm-ubuntu-home <vorametz@gmail.com>
2021-10-14 13:19:27 +0700

    Add more docker cmd

--- docker.sh
+++ docker.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-docker network create bpk-network
+sudo docker network create bpk-network
 
-docker run -d -p 5432:5432 --net bpk-network --name bpk-pg \
+sudo docker run -d -p 5432:5432 --net bpk-network --name bpk-pg \
     -e POSTGRES_DB=testdb \
     -e POSTGRES_USER=postgres \
     -e POSTGRES_PASSWORD=postgres \
     postgres
 
-docker build -t bpk-dx
+sudo docker build -t bpk-dx
 
-docker run -d -p 3000:3000 --net bpk-network --name bpk-c bpk-dx
+sudo docker run -d -p 3000:3000 --net bpk-network --name bpk-c bpk-dx
+
+--restart=always
+sudo docker start `sudo docker ps -q -a` or `-l`
